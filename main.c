2eba8a475325d7b1335e24f12ccf8181c827d559
Amirsobhan Nafariyeh <amirsobhan1553@gmail.com>
2024-04-03 13:58:52 +0330

    Initial commit

---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *handle_conn(void *args) {
+    int* p_c_fd = (int*)args;
+    int c_fd = *p_c_fd;
+
+    char buff[4096];
+    for (;;) {
+        long l = read(c_fd, &buff, 4096 - 1);
+        send(c_fd, &buff, l, 0);
+    }
+}
+
+int main() {
+    int fd, opt;
+
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("socket creation failed");
+        return 1;
+    }
+
+    if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
+        perror("setsockopt failed");
+        return 1;
+    }
+
+    struct sockaddr_in sockaddr;
+    sockaddr.sin_family = AF_INET;
+    sockaddr.sin_addr.s_addr = INADDR_ANY;
+    sockaddr.sin_port = htons(3000);
+
+    if (bind(fd, (const struct sockaddr *) &sockaddr, sizeof(sockaddr)) != 0) {
+        perror("bind failed");
+        return 1;
+    }
+
+    if (listen(fd, 10) != 0) {
+        perror("listen failed");
+        return 1;
+    }
+
+    socklen_t socklen = sizeof(sockaddr);
+    int t_c = 0;
+    pthread_t *pt[10];
+
+    for (;;) {
+        int c_fd;
+        if ((c_fd = accept(fd, (struct sockaddr *) &sockaddr, &socklen)) < 0) {
+            perror("accept failed");
+            break;
+        }
+
+        pthread_t pthread;
+        pt[++t_c] = &pthread;
+        pthread_create(&pthread, NULL, handle_conn, (void *)&c_fd);
+        printf("Thread ID: %lu, Socket: %d\n", pthread, c_fd);
+        fflush(stdout);
+    }
+
+    pthread_exit(NULL);
+}
